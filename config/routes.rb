7e66ff2eae590876acb1b2040a61769cc653aaf9
Mayer <mzseidman@gmail.com>
2014-04-22 16:13:49 -0500

    Users Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Omniauthpls::Application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "users/update"
+  get "users/edit"
+  get "users/destroy"
+  get "users/index"
+  get "users/show"
  get '/auth/:goodreads/callback', :to => 'sessions#create'
 
 end
