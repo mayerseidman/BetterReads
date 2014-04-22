0fdd6fa76bb37294a4726e61fe2d617615a08394
Mayer <mzseidman@gmail.com>
2014-04-22 13:50:24 -0500

    add method to extract info after login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ActiveRecord::Base
+
+client = Goodreads::Client.new(:api_key => 'UpIly3BURwhZ52tmj4ag', :api_secret => 'vIlBCRroZ5Esfn4RRQxY7Sd8sfOx7wOvtWY6thJfqQ')
+
+
+
+
+def self.review = client.review('id')
+
 end
+
+
+def self.from_omniauth(auth, code)
+  if auth.info.image.include? "?sz=50"
+    auth.info.image = auth.info.image.chomp("?sz=50")
+  end
+  where(auth.slice(:provider, :uid)).first_or_create do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.name = auth.info.name
+    user.email = auth.info.email
+    user.auth_code = code
+    user.picture = auth.info.image
+    user.refresh_token = auth.credentials.refresh_token
+  end
+end
+
+end
+
