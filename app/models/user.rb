b4632896094cecbb35b910d0dbaaf3d8c225a774
Mayer <mzseidman@gmail.com>
2014-04-23 17:17:14 -0500

    add root to routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ end
 def self.create_with_omniauth(auth)
   create! do |user|
     user.provider = auth["provider"]
-    user.uid = auth["uid"]
+    # user.uid = auth["uid"]
     user.name = auth["info"]["name"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Omniauthpls::Application.routes.draw do
-  get "users/new"
-  get "users/create"
-  get "users/update"
-  get "users/edit"
-  get "users/destroy"
-  get "users/index"
-  get "users/show"
- get '/auth/:goodreads/callback', :to => 'sessions#create'
+  resources :users
+  # get "users/new"
+  # get "users/create"
+  # get "users/update"
+  # get "users/edit"
+  # get "users/destroy"
+  # get "users/index"
+  # get "users/show"
+ 
+  get '/auth/:goodreads/callback', :to => 'sessions#create'
+  root :to => "users#index"
 
 end
