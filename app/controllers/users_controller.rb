d5de0165697214ad4c1becc7d6e78190db70418f
Mayer <mzseidman@gmail.com>
2014-05-06 18:54:24 -0500

    Connect API and access a given bookby title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,17 +25,17 @@ class UsersController < ApplicationController
     # #                                :site => 'http://www.goodreads.com')
     # # access_token = OAuth::AccessToken.new(consumer, oauth_access_token, oauth_access_secret)
     # # Goodreads::Client.new(oauth_token: access_token, api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
-    Goodreads::Client.new(api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
-    @reviews = client.book_by_title("Steve Jobs")
+    client = Goodreads::Client.new(oauth_token: "ALIVGfWdLZYkh34NIKzVmw", api_key: 'UpIly3BURwhZ52tmj4ag', api_secret: GOODREADS_API_SECRET)
+    @reviews = client.book_by_title("Moby Dick")
   end
 
-# def client
-#     consumer = OAuth::Consumer.new(GOODREADS_API_KEY,
-#                                    GOODREADS_API_SECRET,
-#                                    :site => 'http://www.goodreads.com')
-#     # access_token = OAuth::AccessToken.new(consumer, oauth_access_token, oauth_access_secret)
-#     Goodreads::Client.new(api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
-#   end
+def client
+    consumer = OAuth::Consumer.new(GOODREADS_API_KEY,
+                                   GOODREADS_API_SECRET,
+                                   :site => 'http://www.goodreads.com')
+    # access_token = OAuth::AccessToken.new(consumer, oauth_access_token, oauth_access_secret)
+    Goodreads::Client.new(oauth_token: "ALIVGfWdLZYkh34NIKzVmw", api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
+  end
 
   def show
   end
