17f6b2628723a7716de44f8587e4b0d711bd7f06
Mayer <mzseidman@gmail.com>
2014-05-08 17:22:07 -0500

    removed excess code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,6 @@ class UsersController < ApplicationController
 
   def index
     @user = User.first
-    # client = Goodreads::Client.new(GOODREADS_API_KEY => 'UpIly3BURwhZ52tmj4ag',
-    # GOODREADS_API_SECRET => 'vIlBCRroZ5Esfn4RRQxY7Sd8sfOx7wOvtWY6thJfqQ')
-    # #  consumer = OAuth::Consumer.new(GOODREADS_API_KEY,
-    # #                                GOODREADS_API_SECRET,
-    # #                                :site => 'http://www.goodreads.com')
-    # # access_token = OAuth::AccessToken.new(consumer, oauth_access_token, oauth_access_secret)
-    # # Goodreads::Client.new(oauth_token: access_token, api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
     client = Goodreads::Client.new(oauth_token: "ALIVGfWdLZYkh34NIKzVmw", api_key: 'UpIly3BURwhZ52tmj4ag', api_secret: GOODREADS_API_SECRET)
      # @reviews = client.book_by_title("Moby Dick")
      @group_list = client.group_list(11807848 , 'sort')
