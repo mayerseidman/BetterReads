90c939d1dba9d18b86336745e2bda8f6b9af5a5b
Mayer <mzseidman@gmail.com>
2014-05-18 21:52:22 -0500

    deleted extra client

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,14 +63,6 @@ class UsersController < ApplicationController
 
 end
 
-  def client
-    consumer = OAuth::Consumer.new(GOODREADS_API_KEY,
-                                   GOODREADS_API_SECRET,
-                                   :site => 'http://www.goodreads.com')
-     access_token = OAuth::AccessToken.new(consumer, oauth_access_token, oauth_access_secret)
-    Goodreads::Client.new(oauth_token: "ALIVGfWdLZYkh34NIKzVmw", api_key: GOODREADS_API_KEY, api_secret: GOODREADS_API_SECRET)
-  end
-
   def show
   end
 
