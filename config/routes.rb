700fe2abafbd76ee75b3a9683ac1ab7093ae140b
Mayer <mzseidman@gmail.com>
2014-05-22 01:38:52 -0500

    change home page to new route and cleaned up applic.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Omniauthpls::Application.routes.draw do
-  get "group/index"
-  get "group/show"
+ 
   resources :users
  
   get '/auth/:goodreads/callback', :to => 'sessions#create'
-  root :to => "users#show"
+  root :to => "users#create"
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get 'group', to: 'users#index', as: 'group'
+  get 'groups', to: 'group#index', as: 'groups'
+  get 'group' , to: 'group#show', as: 'group'
 
 end
