81f83cb7fa07b09b96beb03124bd5275522b7223
Mayer <mzseidman@gmail.com>
2014-05-21 23:48:04 -0500

    add indiv groups and turning them into links

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,15 +2,6 @@ class GroupController < ApplicationController
   def index
     client = Goodreads::Client.new(oauth_token: "ALIVGfWdLZYkh34NIKzVmw", api_key: 'UpIly3BURwhZ52tmj4ag', api_secret: GOODREADS_API_SECRET)
     @group_list = client.group_list(current_user.uid, 'sort')
-
-    @group_list.group.each do |g|
-  @group_id = g.id                 # => group id
-  g.access             # => access settings (private, public)
-  g.users_count        # => number of members
-  @group_title = g.title              # => title
-  g.image_url          # => url of the group's image
-  g.last_activity_at   # => date and time of the group's last activity
-end
   end
 
   def show
