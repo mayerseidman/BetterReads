4a931c7307ed35da6cc3ea3d47510fd7877e5c22
Mayer <mzseidman@gmail.com>
2014-05-24 21:49:59 -0500

    add styling and overlay to this branch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider(auth["provider"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, notice: "You have successfully logged in."
+    redirect_to listgroups_path, notice: "You have successfully logged in."
   end
 
   def destroy
@@ -17,4 +17,3 @@ class SessionsController < ApplicationController
 end
   
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Omniauthpls::Application.routes.draw do
+
+ 
   resources :users
-  # get "users/new"
-  # get "users/create"
-  # get "users/update"
-  # get "users/edit"
-  # get "users/destroy"
-  # get "users/index"
-  # get "users/show"
  
   get '/auth/:goodreads/callback', :to => 'sessions#create'
   root :to => "users#show"
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'groups', to: 'groups#index', as: 'listgroups'
+  get 'group' , to: 'groups#show', as: 'group'
 
 end
