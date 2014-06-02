daa06a02b5b4fdbe173538c70d45ee438a09a90a
Mayer <mzseidman@gmail.com>
2014-06-01 22:34:04 -0500

    uppercase User

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   require 'open-uri'
 
   def index
-     @user = user.find(current_user.id)
+     @user = User.find(current_user.id)
     client = Goodreads::Client.new(oauth_token: @user.oauth_token, api_key: 'UpIly3BURwhZ52tmj4ag', api_secret: GOODREADS_API_SECRET)
     @group_list = client.group_list(@user.id, 'sort')
     @group_list.group.each do |g| 
