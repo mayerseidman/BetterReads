570d8df04af6bae0c3077318d3e5205c0d67a871
Mayer <mzseidman@gmail.com>
2014-10-26 11:53:37 -0700

    fixes bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
         @user = User.find(id)
         client = Goodreads::Client.new(oauth_token: @user.oauth_token, api_key: 'UpIly3BURwhZ52tmj4ag', api_secret: GOODREADS_API_SECRET)
         @group_list = client.group_list(@user.id, 'sort')
-
+puts "ola"
         unless @group_list.group.nil? 
             @group_list.group.each do |g|
                 x = g.users_count.to_i/10 
@@ -38,31 +38,52 @@ class User < ActiveRecord::Base
 
                     unless g.users_count.to_i <= @group_record.users.count
                         group.each do |id|
+                            user = User.where(id: id).first
+                            if user
+                                puts "already exists"
+                                unless user.groups.include?(@group_record)
+                                    user.groups << @group_record
+                                    user.save!
+                                end
+                                next
+                            else
+                                puts "new user"    
+                            end
                           begin
                             sleep 2
-                            url = "https://www.goodreads.com/user/show/#{id}.xml?key=01QcdA8pt51gOUi4UJj6A"
-                            dic = Nokogiri::HTML(open(url))
+
+                            @url = "https://www.goodreads.com/user/show/#{id}.xml?key=01QcdA8pt51gOUi4UJj6A"
+                            dic = Nokogiri::HTML(open(@url))
                             @name = dic.xpath("//name")[0].text
                             place = dic.xpath("//location").text
                             location = Location.where(city: place).first_or_create(coordinates: (Geocoder.coordinates place).to_s)
-                            coordinates = location.coordinates
-                            if coordinates.present? && coordinates != "[37.09024, -95.712891]"
+                            @coordinates = location.coordinates
+                            
+                            if @coordinates.present? && @coordinates != "[37.09024, -95.712891]"
+                               
                                 def check_location
-                                    if @group_record.users.collect {|u| u.coordinates}.include?(coordinates)
-                                        coordinates = coordinates.sub(coordinates[5], coordinates[5].next)
+                                    if @group_record.users.collect {|u| u.coordinates}.include?(@coordinates)
+                                        s2 = @coordinates[-4].succ
+                                        @coordinates[-4] = s2
+                                     
                                         check_location
                                     end
                                 end
+
                                 check_location
-                        
-                                user = User.where(id: id).first_or_create(name: @name, coordinates: coordinates, city: location.city)
+                                user = User.where(id: id).first_or_create(name: @name, coordinates: @coordinates, city: location.city)
                                 
                                 unless user.groups.include?(@group_record)
                                     user.groups << @group_record
                                 end
                                 user.save!
+                            else
+                                puts "no location"
                             end
-                          rescue
+                          rescue OpenURI::HTTPError => e 
+                            puts "404 again #{@url}"
+                          rescue Exception => e 
+                            puts "## error #{e.message}"
                           end
                         end
                     end    
